3c8cbdfb2334342f86ff5452f1955eddede1c7a3
Code Help Tutor <1823890830@qq.com>
2024-01-13 11:54:18 +0000

    WeChat cstutorcs cs126

---
+++ src/test/com/example/JSONTest.java
@@ -0,0 +1,23 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package com.example;
+
+
+import com.google.gson.Gson;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+//It may be a good idea to rename/refactor depending on the focus of your assignment.
+public class JSONTest {
+    @Before
+    public void setUp() {
+        // This is run before every test.
+
+    }
+
+    @Test
+    public void sanityCheck() {
+    }
+}
